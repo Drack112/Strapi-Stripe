a3169dcf3ce7c35a75fb7348e7d029a4ddc2e3bb
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-02 12:20:44 -0300

    ⏰ feat: move js file to jsx file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
