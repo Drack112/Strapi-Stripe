ba3eaee86959aa378c1f94c2fa4d0f85654bacb2
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-02 12:39:59 -0300

    🌍 chore: Setup react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './components/Home/Home'
+import Category from './components/Category/Category'
+import SingleProduct from './components/SingleProduct/SingleProduct'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+      </Routes>
     </BrowserRouter>
   )
 }
