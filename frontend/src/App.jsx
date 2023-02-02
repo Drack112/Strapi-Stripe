6c44b8f7695acc61d7f05878fd4d7d64a25d50de
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-02 12:41:04 -0300

    👣 feat: Header Creation component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home/Home'
+import Header from './components/Header/Header'
 import Category from './components/Category/Category'
 import SingleProduct from './components/SingleProduct/SingleProduct'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category/:id" element={<Category />} />
