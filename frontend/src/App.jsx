81484fe06f29d5e97313bd1d2f9bfe1e60f2f581
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-02 12:43:44 -0300

    🎁 feat: Newsletter footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home'
 import Header from './components/Header/Header'
 import Category from './components/Category/Category'
 import SingleProduct from './components/SingleProduct/SingleProduct'
+import NewsLetter from './components/Footer/Newsletter/Newsletter'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/category/:id" element={<Category />} />
         <Route path="/product/:id" element={<SingleProduct />} />
       </Routes>
+      <NewsLetter />
       <Footer />
     </BrowserRouter>
   )
