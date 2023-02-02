5eb685b0c04f80a60cabb9840e28439ce0285cae
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-02 12:47:23 -0300

    ✍️ feat: creation of AppContext with createContext of react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,21 @@ import Category from './components/Category/Category'
 import SingleProduct from './components/SingleProduct/SingleProduct'
 import NewsLetter from './components/Footer/Newsletter/Newsletter'
 
+import AppContext from './utils/context'
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category/:id" element={<Category />} />
-        <Route path="/product/:id" element={<SingleProduct />} />
-      </Routes>
-      <NewsLetter />
-      <Footer />
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+        </Routes>
+        <NewsLetter />
+        <Footer />
+      </AppContext>
     </BrowserRouter>
   )
 }
