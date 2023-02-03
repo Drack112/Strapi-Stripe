9017a79f20c104b72b92b79480d375180ccbd46f
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-03 13:15:56 -0300

    ➕ feat: update header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { TbSearch } from 'react-icons/tb'
 import { CgShoppingCart } from 'react-icons/cg'
 import { AiOutlineHeart } from 'react-icons/ai'
+
 import './Header.scss'
+
 import Search from './Search/Search'
+import { Context } from '../../utils/context'
+import Cart from '../Cart/Cart'
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false)
@@ -23,24 +27,32 @@ const Header = () => {
     window.addEventListener('scroll', handleScroll)
   }, [])
 
+  const { cartCount, showCart, setShowCart } = useContext(Context)
+
   return (
     <>
       <header className={`main-header ${scrolled ? 'sticky-header' : ''}`}>
-        <ul className="left">
-          <li onClick={() => navigate('/')}>Home</li>
-          <li onClick={() => navigate('/about')}>About</li>
-          <li>Categories</li>
-        </ul>
-        <div className="center" onClick={() => navigate('/')}>
-          JSDEVSTORE.
-        </div>
-        <div className="right">
-          <TbSearch size={20} onClick={() => setSearchModal(true)} />
-          <AiOutlineHeart size={20} />
-          <CgShoppingCart size={20} />
+        <div className="header-content">
+          <ul className="left">
+            <li onClick={() => navigate('/')}>Home</li>
+            <li onClick={() => navigate('/about')}>About</li>
+            <li>Categories</li>
+          </ul>
+          <div className="center" onClick={() => navigate('/')}>
+            JSDEVSTORE.
+          </div>
+          <div className="right">
+            <TbSearch onClick={() => setSearchModal(true)} />
+            <AiOutlineHeart />
+            <span className="cart-icon" onClick={() => setShowCart(true)}>
+              <CgShoppingCart />
+              {!!cartCount && <span>{cartCount}</span>}
+            </span>
+          </div>
         </div>
       </header>
       {searchModal && <Search setSearchModal={setSearchModal} />}
+      {showCart && <Cart />}
     </>
   )
 }
