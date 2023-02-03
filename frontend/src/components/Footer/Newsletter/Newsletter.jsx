adf1a37901f8fa1118bc0045eed314829a250eaa
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-03 11:47:07 -0300

    🌱 feat: Newsletter component with styles on scss

--- frontend/src/components/Footer/Newsletter/Newsletter.jsx
+++ frontend/src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,45 @@
+import React from 'react'
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from 'react-icons/fa'
+
 import './Newsletter.scss'
+
 const Newsletter = () => {
-  return <div>Newsletter</div>
+  return (
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">Newsletter</span>
+        <span className="big-text">
+          SignUp for the latest updates and offers
+        </span>
+        <div className="form">
+          <input type="text" placeholder="Email Address" />
+          <button>Subscribe</button>
+        </div>
+        <div className="text">
+          Will be used in accordance with out Privacy Policy
+        </div>
+        <div className="social-icons">
+          <div className="icon">
+            <FaFacebookF size={14} />
+          </div>
+          <div className="icon">
+            <FaTwitter size={14} />
+          </div>
+          <div className="icon">
+            <FaInstagram size={14} />
+          </div>
+          <div className="icon">
+            <FaLinkedinIn size={14} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Newsletter
