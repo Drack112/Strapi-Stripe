01b8f54ff97bd55e5e2715c188bd3b12a14eee11
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-02 23:33:11 -0300

    ⛲️ feat: apply useState to create sticky animation on the header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import './Header.scss'
 
@@ -7,9 +7,14 @@ import { CgShoppingCart } from 'react-icons/cg'
 import { AiOutlineHeart } from 'react-icons/ai'
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false)
   const handleScroll = () => {
     const offset = window.scrollY
-    console.log(offset)
+    if (offset > 200) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
   }
 
   useEffect(() => {
@@ -17,7 +22,7 @@ const Header = () => {
   }, [])
 
   return (
-    <header className="main-header">
+    <header className={`main-header ${scrolled ? 'sticky-header' : ''}`}>
       <div className="header-content">
         <ul className="left">
           <li>Home</li>
