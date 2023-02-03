33d188a985caafd04afdfa29ca43b2cfea0b6c6f
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-03 12:05:14 -0300

    🏠 feat: category component on home page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import './Home.scss'
 
 import Banner from './Banner/Banner'
+import Category from './Category/Category'
 
 const Home = () => {
   return (
     <div className="home">
       <Banner />
+      <Category />
     </div>
   )
 }
