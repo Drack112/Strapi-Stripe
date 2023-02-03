eca54c327ff95b18fc86a956ef92277632f7f136
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-02 22:15:31 -0300

    🎨 feat: style and icons on header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,30 @@
 import './Header.scss'
+
+import { TbSearch } from 'react-icons/tb'
+import { CgShoppingCart } from 'react-icons/cg'
+import { AiOutlineHeart } from 'react-icons/ai'
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header class="main-header">
+      <div className="header-content">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Categories</li>
+        </ul>
+        <div className="center">JSDEVSTORE.</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon">
+            <CgShoppingCart />
+            <span>5</span>
+          </span>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
