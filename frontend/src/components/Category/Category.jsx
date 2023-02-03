7f7eb21fc7dd6f269df62d5aec64eba0e00a563f
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-03 13:16:23 -0300

    🌞 feat: update category component

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -1,6 +1,28 @@
+import { useParams } from 'react-router-dom'
+
+import useFetch from '../../../strapi-simple-one-client/src/hooks/useFetch'
+import Products from '../Products/Products'
+
 import './Category.scss'
+
 const Category = () => {
-  return <div>Category</div>
+  const { id } = useParams()
+  const { data } = useFetch(
+    `/api/products?populate=*&[filters][categories][id]=${id}`,
+  )
+  return (
+    <div className="category-main-content">
+      <div className="layout">
+        <div className="category-title">
+          {
+            data?.data?.[0]?.attributes?.categories?.data?.[0]?.attributes
+              ?.title
+          }
+        </div>
+        <Products innerPage={true} products={data} />
+      </div>
+    </div>
+  )
 }
 
 export default Category
