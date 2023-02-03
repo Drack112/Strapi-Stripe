5a56a60e9835888b683481ac18b668b67f2d58b5
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-03 10:37:29 -0300

    🌞 feat: base style and update of the component

--- frontend/src/components/Home/Banner/Banner.jsx
+++ frontend/src/components/Home/Banner/Banner.jsx
@@ -5,7 +5,18 @@ const Banner = () => {
   return (
     <div className="hero-banner">
       <div className="content">
-        <div className="text-content"></div>
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Take te best of our headphones and more! Make your music be so real
+            than the galaxy. With the new collection, you can surf on the waves
+            of the pop and the fire of the rock.
+          </p>
+          <div className="cta">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v">Shop Now</div>
+          </div>
+        </div>
         <img src={BannerImg} alt="Banner image of headphone" />
       </div>
     </div>
