5c979d357ba3b35d2296ca4c0c6b0164937156bf
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-03 13:15:38 -0300

    🧩 feat: update footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
 import './Footer.scss'
 
 import { FaLocationArrow, FaMobileAlt, FaEnvelope } from 'react-icons/fa'
+
 import Payment from '../../assets/payments.png'
 
 const Footer = () => {
